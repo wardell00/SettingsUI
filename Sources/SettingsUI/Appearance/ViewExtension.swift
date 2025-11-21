bb2b6425b0bdbc05b4fe03aa1df7f79d23c8d828
Alexander Loewe <loewea2000@gmail.com>
2025-11-21 13:18:27 +0100

    Added a view extension for appearance

---
+++ Sources/SettingsUI/Appearance/ViewExtension.swift
@@ -0,0 +1,15 @@
+//
+//  Untitled.swift
+//  SettingsUI
+//
+//  Created by Alexander Löwe on 21.11.25.
+//
+
+import SwiftUI
+import SettingsUI_Internal
+
+public extension View {
+    func changeAppearance() -> some View {
+        _changeAppearance()
+    }
+}
