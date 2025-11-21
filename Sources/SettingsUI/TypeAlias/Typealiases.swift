f8f52512dcb8f8180ec1406369a804885e37975a
Alexander Loewe <loewea2000@gmail.com>
2025-11-21 13:18:50 +0100

    Added typealiases

---
+++ Sources/SettingsUI/TypeAlias/Typealiases.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  SettingsUI
+//
+//  Created by Alexander Löwe on 21.11.25.
+//
+
+import SwiftUI
+import SettingsUI_Internal
+
+public typealias URLLink = _URLLink
+public typealias AccentColorTint = _AccentColorTint
+public typealias Appearance = _Appearance
+public typealias SettingsValues = _SettingsValues
+public typealias AppStorageKey = _AppStorageKey
